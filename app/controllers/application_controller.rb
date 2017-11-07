40397465278eb3f739b4732c05f210f944be5d9d
Tamires Santos <tamtamsantos@gmail.com>
2017-11-07 16:43:55 -0300

    Custom Devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
-  include CanCan::ControllerAditions
+  # include CanCan::ControllerAditions
 end
