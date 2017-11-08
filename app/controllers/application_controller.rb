06c355a6bd8ac50f1263c315e696af43aa1fcc16
Tamires Santos <tamtamsantos@gmail.com>
2017-11-07 22:33:24 -0300

    fix button on Devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include CanCan::ControllerAdditions
   protect_from_forgery with: :exception
-
-  # include CanCan::ControllerAditions
 end
