6efb97dfdafd671a34109bb7249cc4c1b437d1bf
Tamires Santos <tamtamsantos@gmail.com>
2017-11-10 13:34:08 -0300

    Improve models

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,5 +2,7 @@ class Channel < ApplicationRecord
   has_many :messages, as: :messagable, :dependent => :destroy
   belongs_to :team
   belongs_to :user
+  
   validates_presence_of :slug, :team, :user
+  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
