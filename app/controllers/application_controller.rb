3b320ab1d535dbe593a4c1df4a113bced5215162
Tamires Santos <tamtamsantos@gmail.com>
2017-11-10 13:55:20 -0300

    cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include CanCan::ControllerAdditions
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
+
 end
